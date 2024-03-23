487db4e0c626d88bdf062a421fc4d84e641c0dfa
StefanoRucci <stefano.rucci981@gmail.com>
2024-03-23 15:39:54 +0100

    Ottimizzazione caricamento

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useIsFocused } from "@react-navigation/native";
 import PlacesList from "../components/Places/PlacesList";
 import { fetchPlaces } from "../util/http";
@@ -13,38 +13,25 @@ function AllPlaces({ route }) {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
   const [isFetching, setIsFetching] = useState(true);
-  const [firstLoad, setFirstLoad] = useState(true); // Nuovo stato per il primo caricamento
-  const [gradientLoaded, setGradientLoaded] = useState(false); // Stato per tracciare il caricamento del gradiente
+  const [firstLoad, setFirstLoad] = useState(true);
   const isFocused = useIsFocused();
 
   const authCtx = useContext(AuthContext);
   const token = authCtx.token;
 
-  const getPlaces = async () => {
-    try {
-      setIsFetching(true);
-      const places = await fetchPlaces(token);
-      setIsFetching(false);
-      setLoadedPlaces(places);
-      setRefreshing(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
-    const loadGradient = async () => {
-      // Simula un ritardo nel caricamento del gradiente
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      setGradientLoaded(true);
+    const getPlaces = async () => {
+      try {
+        setIsFetching(true);
+        const places = await fetchPlaces(token);
+        setLoadedPlaces(places);
+        setIsFetching(false);
+        setRefreshing(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-    if (!gradientLoaded) {
-      loadGradient();
-    }
-  }, [gradientLoaded]);
-
-  useEffect(() => {
     if (isFocused && (firstLoad || refreshing)) {
       setRefreshing(true);
       getPlaces();
@@ -54,15 +41,13 @@ function AllPlaces({ route }) {
 
   const onRefresh = () => {
     setRefreshing(true);
-    getPlaces();
   };
 
-  if (!gradientLoaded) {
-    return <LoadingOverlay message="Loading ..." />;
-  }
-
   return (
-    <LinearGradient colors={[Colors.gray700, Colors.primary800]}>
+    <LinearGradient
+      colors={[Colors.gray700, Colors.primary800]}
+      style={{ flex: 1 }}
+    >
       <PlacesList
         places={loadedPlaces}
         refreshing={refreshing}
