6fc93ad0909a635ad54f9a7e430c867ece646cd4
StefanoRucci <101905119+StefanoRucci@users.noreply.github.com>
2024-04-02 23:06:20 +0200

    Update location.js

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = <Inserisci api_key>;
+const GOOGLE_API_KEY = "<Inserisci api_key>";
 
 export const getMapPreview = (lat, lng) => {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
